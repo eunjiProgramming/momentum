84a962cfc026d28e26c6b3448492e7aac6990700
eunjiProgramming <estelle.programming@gmail.com>
2023-10-24 12:05:43 +0900

    #4.1 Form Submission

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ const loginInput = document.querySelector('#login-form input');
 const loginButton = document.querySelector('#login-form button');
 
 function onLoginBtnClick() {
-  console.log(loginInput.value);
-  console.log('hi');
+  const username = loginInput.value;
+  console.log(username);
 }
 
 loginButton.addEventListener('click', onLoginBtnClick);
